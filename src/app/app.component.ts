e02c93223ae692ec9e70069e2df3c4741df33375
Pol <polcerdan@gmail.com>
2019-05-14 21:27:17 +0200

    first commit

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,7 +1,7 @@
 import { Component } from '@angular/core';
 
 @Component({
-  selector: 'cf-root',
+  selector: 'cf-raiz',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.scss']
 })
